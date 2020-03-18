d4716822c3094b593a3e916a7dc7b18ae9f2e0b6
olahdome <olah.dome@gmail.com>
2020-03-18 20:43:00 +0100

    First commit

---
+++ app/src/main/java/com/example/blank/di/ApplicationModule.kt
@@ -0,0 +1,9 @@
+package com.example.blank.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class ApplicationModule(@get:Provides @Singleton val context: Context)
---
+++ app/src/main/java/com/example/blank/di/ViewModelModule.kt
@@ -0,0 +1,19 @@
+package com.example.blank.di
+
+import androidx.lifecycle.ViewModel
+import co.zsmb.rainbowcake.dagger.ViewModelKey
+import com.example.blank.ui.blank.BlankViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+@Suppress("unused")
+@Module
+abstract class ViewModelModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(BlankViewModel::class)
+    abstract fun bindBlankViewModel(blankViewModel: BlankViewModel): ViewModel
+
+}
